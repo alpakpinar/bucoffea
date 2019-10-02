706a6da6d68f7cdbffef00b5b7465b652bae0190
Alp Akpinar <aakpinar@bu.edu>
2019-10-01 20:59:36 -0500

    Added linear-y axis option

--- bucoffea/scripts/print_plots.py
+++ bucoffea/scripts/print_plots.py
@@ -15,7 +15,7 @@ def commandline():
 def main():
     args = commandline()
     acc = load(args.file)
-    debug_plot_output(acc, args.region, args.outpath)
+    debug_plot_output(acc, args.region, args.outpath, logscaley=False)
 
 if __name__ == "__main__":
     main()
