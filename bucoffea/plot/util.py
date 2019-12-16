1b5b560b276d6d42fd8e30f30a5f4057aacdd046
Alp Akpinar <aakpinar@bu.edu>
2019-12-16 10:24:02 -0600

    Add GJets-DR datasets

--- bucoffea/plot/util.py
+++ bucoffea/plot/util.py
@@ -179,6 +179,11 @@ def merge_datasets(histogram):
         'GJets_HT_MLM_2016' : [x for x in all_datasets if re.match('GJets_HT-(\d+)To.*-MLM_2016',x)],
         'GJets_HT_MLM_2017' : [x for x in all_datasets if re.match('GJets_HT-(\d+)To.*-MLM_2017',x)],
         'GJets_HT_MLM_2018' : [x for x in all_datasets if re.match('GJets_HT-(\d+)To.*-MLM_2018',x)],
+        
+        'GJets_DR-0p4_HT_MLM_2017' : [x for x in all_datasets if re.match('GJets_DR-0p4_HT-(\d+)To.*-MLM_2017',x)],
+        'GJets_DR-0p4_HT_MLM_2018' : [x for x in all_datasets if re.match('GJets_DR-0p4_HT-(\d+)To.*-MLM_2018',x)],
+        'GJets_DR-0p4_HT_MLM_v2_2017' : [x for x in all_datasets if re.match('GJets_DR-0p4_HT-(\d+)To.*-MLM_v2_2017',x)],
+        'GJets_DR-0p4_HT_MLM_v2_2018' : [x for x in all_datasets if re.match('GJets_DR-0p4_HT-(\d+)To.*-MLM_v2_2018',x)],
 
         'AGJets_SM_5f_EWK-mg_2017' : ['GJets_SM_5f_EWK-mg_2017'],
         'AGJets_SM_5f_EWK-mg_2018' : ['GJets_SM_5f_EWK-mg_2017'],
