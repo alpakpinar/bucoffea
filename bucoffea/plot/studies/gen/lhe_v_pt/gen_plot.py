bdf1ac6a0afefc877dc332132796e14b367058cb
Alp Akpinar <aakpinar@bu.edu>
2020-01-13 23:03:23 +0300

    Remove unneccessary prints

--- bucoffea/plot/studies/gen/lhe_v_pt/gen_plot.py
+++ bucoffea/plot/studies/gen/lhe_v_pt/gen_plot.py
@@ -116,7 +116,6 @@ def plot_2d_gen_spectrum(acc, tag, regex, outputrootfile, pt_type='stat1', sampl
     histogram = histogram.integrate('jpt')
     histogram = histogram.rebin(histogram.axis('vpt'), _REBIN_AX['vpt'])
     histogram = histogram.rebin(histogram.axis('mjj'), _REBIN_AX['mjj'])
-    print(histogram.values())
 
     if sample_order == 'lo':
         histogram = histogram[re.compile('.*HT.*')].integrate('dataset') 
@@ -124,7 +123,6 @@ def plot_2d_gen_spectrum(acc, tag, regex, outputrootfile, pt_type='stat1', sampl
         histogram = histogram[re.compile('.*(LHE|amcat).*')].integrate('dataset') 
     else:
         raise ValueError(f'{sample_order}: Invalid argument for sample_order. Should be specified as "lo" or "nlo"')
-    print(histogram.values())
 
     # Plot the 2D histogram and save the figure
     fig, ax = plt.subplots(1,1, figsize=(7,5))
