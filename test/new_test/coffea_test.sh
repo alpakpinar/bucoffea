fb59b2fe0e9ef4bcace5a6932dc9d07c17f6ed6f
Alp Akpinar <aakpinar@bu.edu>
2020-06-07 05:09:15 -0500

    Script fixes, try adding pytest

--- test/new_test/coffea_test.sh
+++ test/new_test/coffea_test.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 # First, run the VBF processor to get coffea output via run_quick.py script
-cd bucoffea/scripts;
+cd ../../bucoffea/scripts;
 echo "===================="
 echo "Running run_quick.py"
 echo "===================="
 ./run_quick.py vbfhinv
 
+# Get the coffea output from run_quick, feed it into the checking script
+
+outputfilename="run_quick_output.txt"
+run_quick_output=$(head -n 1 ${outputfilename})
 # Then run the python testing script on the output
+echo "===================="
+echo "Running coffea checker"
+echo "===================="
+echo "I am here:"
 cd -;
-cd test/new_test;
-python ./check_coffea_histograms.py
+py.test 
+# python ./check_coffea_histograms.py ${run_quick_output}
 
 
