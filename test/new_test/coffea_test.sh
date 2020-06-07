c51d30d382a2eb05cdcf66c8ea50c935b0f0e6dc
Alp Akpinar <aakpinar@bu.edu>
2020-06-07 06:26:17 -0500

    Restructure the shell script

--- test/new_test/coffea_test.sh
+++ test/new_test/coffea_test.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
 # First, run the VBF processor to get coffea output via run_quick.py script
-cd ../../bucoffea/scripts;
-echo "===================="
-echo "Running run_quick.py"
-echo "===================="
-./run_quick.py vbfhinv
+# cd ../../bucoffea/scripts;
+# echo "===================="
+# echo "Running run_quick.py"
+# echo "===================="
+# ./run_quick.py vbfhinv
+
+while IFS=" " read -r DATASET REMOTE_PATH REMAINDER
+do
+    FNAME=${DATASET}.root
+    if [[ ! -f $FNAME ]]; then
+        wget ${REMOTE_PATH}/${FNAME}
+    fi
+    echo ${FNAME} > files.txt
+    buexec vbfhinv --outpath ./output/ --jobs 1 worker --dataset ${DATASET} --filelist files.txt --chunk 0
+done < "testfiles_new.txt"
 
 # Get the coffea output from run_quick, feed it into the checking script
 
-outputfilename="run_quick_output.txt"
-run_quick_output=$(head -n 1 ${outputfilename})
+# outputfilename="run_quick_output.txt"
+# run_quick_output=$(head -n 1 ${outputfilename})
 # Then run the python testing script on the output
 echo "===================="
 echo "Running coffea checker"
 echo "===================="
-echo "I am here:"
-cd -;
-py.test 
-# python ./check_coffea_histograms.py ${run_quick_output}
+cd output;
+for file in vbfhinv_W*2017.coffea
+do
+    pytest ./check_coffea_histograms.py ${file}
+done
 
 
