c8c3662c2724aba80a93b1d73c0e1f6e117c63ba
Alp Akpinar <aakpinar@bu.edu>
2020-06-10 18:23:42 +0300

    Add script to plot split JES uncertainties on transfer factors

---
+++ bucoffea/plot/studies/jes_jer_variation/plot_split_jes_ratio.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+
+# -------------------------
+# Script for plotting split JES uncertainties
+# -------------------------
+
+import os
+import sys
+import re
+import argparse
+from bucoffea.plot.util import merge_datasets, merge_extensions, scale_xs_lumi, fig_ratio
+from klepto.archives import dir_archive
+from coffea import hist
+from matplotlib import pyplot as plt
+import matplotlib.ticker
+import mplhep as hep
+import numpy as np
+# import pandas as pd
+from pprint import pprint
+from itertools import chain
+
+pjoin = os.path.join
+
+titles = {
+    'GluGlu2017' : r'$ggH(inv) \ 2017$ split JEC uncertainties',
+    'GluGlu2018' : r'$ggH(inv) \ 2018$ split JEC uncertainties',
+    'VBF2017' : r'$VBF \ H(inv) \ 2017$ split JEC uncertainties',
+    'VBF2018' : r'$VBF \ H(inv) \ 2018$ split JEC uncertainties',
+    'ZJets2016' : r'$Z(\nu\nu) \ 2016$ split JEC uncertainties',
+    'ZJets2017' : r'$Z(\nu\nu) \ 2017$ split JEC uncertainties'
+}
+
+titles_two_nuisances = {
+    'GluGlu2017' : r'$ggH(inv) \ 2017$ corr vs uncorr JEC uncertainties',
+    'GluGlu2018' : r'$ggH(inv) \ 2018$ corr vs uncorr JEC uncertainties',
+    'VBF2018' : r'$VBF \ H(inv) \ 2018$ corr vs uncorr JEC uncertainties',
+    'VBF2017' : r'$VBF \ H(inv) \ 2017$ corr vs uncorr JEC uncertainties',
+    'ZJets2016' : r'$Z(\nu\nu) \ 2016$ corr vs uncorr JEC uncertainties',
+    'ZJets2017' : r'$Z(\nu\nu) \ 2017$ corr vs uncorr JEC uncertainties'
+}
+
+# Define all possible binnings for all variables in this dictionary
+mjj_binning_v1 = hist.Bin('mjj', r'$M_{jj} \ (GeV)$', list(range(200,800,300)) + list(range(800,2000,400)) + [2000, 2750, 3500])
+mjj_binning_single_bin = hist.Bin('mjj', r'$M_{jj} \ (GeV)$', [200,3500])
+
+met_binning_v1_2016 = hist.Bin('met', r'$MET \ (GeV)$', list(range(0,500,50)) + list(range(500,1100,100))) 
+met_binning_v1_2017 = hist.Bin('met', r'$MET \ (GeV)$', list(range(0,500,100)) + list(range(500,1250,250))) 
+met_binning_single_bin = hist.Bin('met', r'$MET \ (GeV)$', [200,1500])
+met_binning_coarse = hist.Bin('met', r'$MET \ (GeV)$', [250,300,400,500,800,1500])
+
+binnings = {
+    'met' : {
+        'initial' : {'2016' : met_binning_v1_2016, '2017': met_binning_v1_2017, '2018' : met_binning_v1_2017},
+        'single bin' : {'2016' : met_binning_single_bin, '2017' : met_binning_single_bin, '2018' : met_binning_single_bin},
+        'coarse' : {'2016' : met_binning_coarse, '2017' : met_binning_coarse, '2018': met_binning_coarse}
+    },
+    'mjj' : {
+        'initial' : {'2016' : mjj_binning_v1, '2017': mjj_binning_v1, '2018': mjj_binning_v1},
+        'single bin' : {'2016': mjj_binning_single_bin, '2017': mjj_binning_single_bin, '2018': mjj_binning_single_bin}
+    }
+}
+
+def parse_cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inpath', help='Path containing merged coffea files.')
+    parser.add_argument('--tag', help='Tag for the transfer factor to be used.')
+    parser.add_argument('--analysis', help='The analysis being considered, default is vbf.', default='vbf')
+    # parser.add_argument('--regroup', help='Construct the uncertainty plot with the sources grouped into correlated and uncorrelated.', action='store_true')
+    parser.add_argument('--ratio', help='Plot uncertainties on transfer factors.', action='store_true')
+    parser.add_argument('--year', help='The year for which the dataset is produced.')
+    args = parser.parse_args()
+    return args
+
+def plot_split_jecunc_ratios(acc, out_tag, tag_num, tag_denom, year, plot_total=True, skimmed=True, bin_selection='initial', analysis='vbf'):
+    '''Plot all split JEC uncertainties on transfer factors in the same plot.'''
+    # Load the relevant variable to analysis, select binning
+    variable_to_use = 'mjj' if analysis == 'vbf' else 'met'
+    acc.load(variable_to_use)
+    h = acc[variable_to_use]
+
+    # Rebin the histogram
+    new_bins = binnings[variable_to_use][bin_selection][year]
+    h = h.rebin(variable_to_use , new_bins)
+
+    h = merge_extensions(h, acc, reweight_pu=False)
+    scale_xs_lumi(h)
+    h = merge_datasets(h)
+
+    region_suffix = '_j' if analysis == 'monojet' else '_vbf'
+
+    # Get the histograms for numerator and denominator
+    h_num = h.integrate('dataset', re.compile(f'{tag_num}.*{year}'))[re.compile(f'sr{region_suffix}.*')]
+    h_den = h.integrate('dataset', re.compile(f'{tag_denom}.*{year}'))[re.compile(f'sr{region_suffix}.*')]
+
+    h_nominal_num = h_num.integrate('region', f'sr{region_suffix}')
+    h_nominal_den = h_den.integrate('region', f'sr{region_suffix}')
+    # Get the nominal ratio and store it in a dict
+    ratios = {}
+    nominal_ratio = h_nominal_num.values()[()] / h_nominal_den.values()[()]
+    ratios['nominal'] = nominal_ratio
+    pprint(nominal_ratio)
+
+    data_err_opts = {
+        'linestyle':'-',
+        'marker': '.',
+        'markersize': 10.,
+        'elinewidth': 1,
+    }
+
+    fig, ax = plt.subplots()
+    centers = h_num.axis(variable_to_use).centers()
+
+    # Look at only certain variations if we are not to plot everything
+    vars_to_look_at = ['jesRelativeBal', f'jesRelativeSample_{year}', 'jesAbsolute', f'jesAbsolute_{year}', 'jesFlavorQCD', 'jesTotal']
+    
+    # Setup the color map
+    colormap = plt.cm.nipy_spectral
+    num_plots = len(vars_to_look_at) if skimmed else 12
+    colors = []
+    for i in np.linspace(0,0.9,num_plots):
+        colors.append([colormap(i), colormap(i)])
+
+    # Flatten the color list
+    colors = list(chain.from_iterable(colors))
+    ax.set_prop_cycle('color', colors)
+
+    for region in h_num.identifiers('region'):
+        if region.name == f'sr{region_suffix}':
+            continue
+        if not plot_total:
+            if "Total" in region.name:
+                continue
+        h_varied_num = h_num.integrate('region', region)
+        h_varied_den = h_den.integrate('region', region)
+        var_label = region.name.replace(f'sr{region_suffix}_', '')
+        var_label_skimmed = re.sub('(Up|Down)', '', var_label)
+
+        # Get the varied ratio and store it
+        varied_ratio = h_varied_num.values()[()] / h_varied_den.values()[()]
+        # ratios[var_label] = varied_ratio
+
+        if skimmed:
+            if var_label_skimmed not in vars_to_look_at:
+                continue
+        dratio = varied_ratio / nominal_ratio
+        ax.plot(centers, dratio, marker='o', label=var_label)
+        # hist.plotratio(h_var, h_nom, ax=ax, clear=False, label=var_label, unc='num',  guide_opts={}, error_opts=data_err_opts)
+
+    fig.savefig('test.pdf')
+
+###########################
+
+def main():
+    args = parse_cli()
+    inpath = args.inpath
+    transfer_factor_tag = args.tag
+    analysis = args.analysis
+    year = args.year
+
+    acc = dir_archive(
+        inpath,
+        serialized=True,
+        compression=0,
+        memsize=1e3
+    )
+
+    acc.load('sumw')
+    acc.load('sumw2')
+
+    if inpath.endswith('/'):
+        out_tag = inpath.split('/')[-2]
+    else:
+        out_tag = inpath.split('/')[-1]
+
+    # Determine the datasets for the transfer factor
+    if transfer_factor_tag == 'zoverw':
+        tag_num = 'ZJets'
+        tag_denom = 'WJets'
+
+    # Ratio plotting to be called here
+    plot_split_jecunc_ratios(acc, out_tag, tag_num, tag_denom, year)
+
+if __name__ == '__main__':
+    main()
