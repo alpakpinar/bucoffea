57bc365ebb2bbcf748df87d5782dcb92f1df6a67
Alp Akpinar <aakpinar@bu.edu>
2020-07-08 07:20:18 -0500

    Plot T1 MET in the comparison plot as well

--- bucoffea/plot/studies/compare_met_pt_jer/compare_met_pt_jer.py
+++ bucoffea/plot/studies/compare_met_pt_jer/compare_met_pt_jer.py
@@ -14,13 +14,18 @@ pjoin = os.path.join
 
 def compare_met_pt_jer(acc_19Feb20, acc_05Jun20v5, process='ZJetsToNuNu', year=2017, region='cr_baseline_vbf'):
     '''Compare smeared MET pt distribution between the two accumulators.'''
+    # Load in the T1Smear and T1 MET (met and met_nom, respectively)
     acc_19Feb20.load('met')
+    acc_19Feb20.load('met_nom')
     acc_05Jun20v5.load('met')
+    acc_05Jun20v5.load('met_nom')
 
-    h_19Feb20   = acc_19Feb20['met']
-    h_05Jun20v5 = acc_05Jun20v5['met']
+    h_19Feb20       = acc_19Feb20['met']
+    h_19Feb20_nom   = acc_19Feb20['met_nom']
+    h_05Jun20v5     = acc_05Jun20v5['met']
+    h_05Jun20v5_nom = acc_05Jun20v5['met_nom']
 
-    def preprocess(h,acc):
+    def preprocess(h,acc,T1MET=False):
         h = merge_extensions(h, acc, reweight_pu=False)
         scale_xs_lumi(h)
         h = merge_datasets(h)
@@ -30,22 +35,43 @@ def compare_met_pt_jer(acc_19Feb20, acc_05Jun20v5, process='ZJetsToNuNu', year=2
 
         # Rebin
         if region == 'cr_baseline_vbf':
-            met_bin = hist.Bin('met',r'$p_{T}^{miss}$ (GeV)',list(range(100,520,20)))
+            if T1MET:
+                met_bin = hist.Bin('met_nom',r'$p_{T}^{miss}$ (GeV)',list(range(100,520,20)))
+            else:
+                met_bin = hist.Bin('met',r'$p_{T}^{miss}$ (GeV)',list(range(100,520,20)))
+        elif region == 'cr_2m_vbf':
+            if T1MET:
+                met_bin = hist.Bin('met_nom',r'$p_{T}^{miss}$ (GeV)',list(range(0,150,10)))
+            else:
+                met_bin = hist.Bin('met',r'$p_{T}^{miss}$ (GeV)',list(range(0,150,10)))
         else:
-            met_bin = hist.Bin('met',r'$p_{T}^{miss}$ (GeV)',list(range(250,550,20)))
+            if T1MET:
+                met_bin = hist.Bin('met_nom',r'$p_{T}^{miss}$ (GeV)',list(range(250,550,20)))
+            else:
+                met_bin = hist.Bin('met',r'$p_{T}^{miss}$ (GeV)',list(range(250,550,20)))
             
-        h = h.rebin('met', met_bin)
+        # Rebin MET (T1 or T1Smear)
+        if T1MET:
+            h = h.rebin('met_nom', met_bin)
+        else:
+            h = h.rebin('met', met_bin)
 
         return h
 
     # Preprocess histograms
     h_19Feb20   = preprocess(h_19Feb20, acc_19Feb20)
     h_05Jun20v5 = preprocess(h_05Jun20v5, acc_05Jun20v5)
+    h_19Feb20_nom   = preprocess(h_19Feb20_nom, acc_19Feb20)
+    h_05Jun20v5_nom = preprocess(h_05Jun20v5_nom, acc_05Jun20v5)
 
     # Plot the comparison
     fig, (ax, rax) = plt.subplots(2, 1, figsize=(7,7), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
+    # Plot T1Smear MET
     hist.plot1d(h_19Feb20, ax=ax)
     hist.plot1d(h_05Jun20v5, ax=ax, clear=False)
+    
+    # Plot T1 MET (just from 05Jun20v5 for now)
+    hist.plot1d(h_05Jun20v5_nom, ax=ax, clear=False)
 
     ax.set_xlabel('')
     # Set the title for the figure
@@ -65,13 +91,16 @@ def compare_met_pt_jer(acc_19Feb20, acc_05Jun20v5, process='ZJetsToNuNu', year=2
         'elinewidth': 1,
     }
 
-    ax.legend(labels=['Without JER fix', 'With JER fix'])
+    ax.legend(labels=['Without JER fix', 'With JER fix', 'T1 MET'])
 
     # Plot ratio
     hist.plotratio(h_05Jun20v5, h_19Feb20, ax=rax, error_opts=data_err_opts, unc='num')
     rax.grid(True)
     rax.set_ylabel('With JER fix / Without')
-    rax.set_ylim(0.8,1.2)
+    if region == 'cr_2m_vbf':
+        rax.set_ylim(0.6,1.4)
+    else:
+        rax.set_ylim(0.8,1.2)
 
     loc = matplotlib.ticker.MultipleLocator(base=0.1)
     rax.yaxis.set_major_locator(loc)
