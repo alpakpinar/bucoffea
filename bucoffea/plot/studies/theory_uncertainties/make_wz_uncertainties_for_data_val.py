ac2d4cfb6089998fc15e605cef1ac358ee9c2eb1
Alp Akpinar <aakpinar@bu.edu>
2020-07-12 06:15:38 -0500

    Change uncertainty naming, add W/gamma uncertainties

--- bucoffea/plot/studies/theory_uncertainties/make_wz_uncertainties_for_data_val.py
+++ bucoffea/plot/studies/theory_uncertainties/make_wz_uncertainties_for_data_val.py
@@ -98,7 +98,6 @@ def from_coffea(inpath, outfile, run_z_w_only=False, run_qcd_only=False, variabl
         for unc in map(str, h_z_ee_unc.axis('uncertainty').identifiers()):
             if not re.match('.*zover(w|g).*', unc):
                 continue
-            print(unc)
             h = h_z_ee_unc.integrate(h_z_ee_unc.axis('uncertainty'), unc)
             f[f'zee_qcd_{variable}_{unc}_{year}'] = export1d(h)
 
@@ -108,7 +107,6 @@ def from_coffea(inpath, outfile, run_z_w_only=False, run_qcd_only=False, variabl
             if not re.match('.*woverg.*', unc):
                 continue
             # pprint(h_z_mumu_unc.identifiers('uncertainty'))
-            print(unc)
             h = h_w_munu_unc.integrate(h_w_munu_unc.axis('uncertainty'), unc)
             f[f'wmunu_qcd_{variable}_{unc}_{year}'] = export1d(h)
             
@@ -118,7 +116,6 @@ def from_coffea(inpath, outfile, run_z_w_only=False, run_qcd_only=False, variabl
             if not re.match('.*woverg.*', unc):
                 continue
             # pprint(h_z_mumu_unc.identifiers('uncertainty'))
-            print(unc)
             h = h_w_enu_unc.integrate(h_w_enu_unc.axis('uncertainty'), unc)
             f[f'wenu_qcd_{variable}_{unc}_{year}'] = export1d(h)
             
@@ -153,7 +150,6 @@ def from_coffea(inpath, outfile, run_z_w_only=False, run_qcd_only=False, variabl
             for unc in map(str, h_z_mumu_ewk_unc.axis('uncertainty').identifiers()):
                 if not re.match('.*zover(w|g).*', unc):
                     continue
-                print(unc)
                 h = h_z_mumu_ewk_unc.integrate(h_z_mumu_ewk_unc.axis('uncertainty'), unc)
                 f[f'zmumu_ewk_{variable}_{unc}_{year}'] = export1d(h)
 
@@ -161,7 +157,6 @@ def from_coffea(inpath, outfile, run_z_w_only=False, run_qcd_only=False, variabl
             for unc in map(str, h_z_ee_ewk_unc.axis('uncertainty').identifiers()):
                 if not re.match('.*zover(w|g).*', unc):
                     continue
-                print(unc)
                 h = h_z_ee_ewk_unc.integrate(h_z_ee_ewk_unc.axis('uncertainty'), unc)
                 f[f'zee_ewk_{variable}_{unc}_{year}'] = export1d(h)
     
@@ -170,7 +165,6 @@ def from_coffea(inpath, outfile, run_z_w_only=False, run_qcd_only=False, variabl
             for unc in map(str, h_w_munu_ewk_unc.axis('uncertainty').identifiers()):
                 if not re.match('.*woverg.*', unc):
                     continue
-                print(unc)
                 h = h_w_munu_ewk_unc.integrate(h_w_munu_ewk_unc.axis('uncertainty'), unc)
                 f[f'wmunu_ewk_{variable}_{unc}_{year}'] = export1d(h)
 
@@ -178,7 +172,6 @@ def from_coffea(inpath, outfile, run_z_w_only=False, run_qcd_only=False, variabl
             for unc in map(str, h_w_enu_ewk_unc.axis('uncertainty').identifiers()):
                 if not re.match('.*woverg.*', unc):
                     continue
-                print(unc)
                 h = h_w_enu_ewk_unc.integrate(h_w_enu_ewk_unc.axis('uncertainty'), unc)
                 f[f'wenu_ewk_{variable}_{unc}_{year}'] = export1d(h)
 
@@ -198,7 +191,7 @@ def make_ratios(infile, run_z_w_only=False, run_qcd_only=False, variable='mjj'):
             denominator_gjets = f.Get(f'gjets_{source}_{variable}_nominal_{year}')
             for name in map(lambda x:x.GetName(), f.GetListOfKeys()):
                 # Z(mumu) / W(munu)
-                if name.startswith(f'zmumu_{source}') and 'zoverw' in name:
+                if name.startswith(f'zmumu_{source}') and ('zoverw' in name or 'nominal' in name):
                     if not f"{year}" in name or 'ewkcorr' in name:
                         continue
                     if 'nominal' in name:
@@ -207,11 +200,11 @@ def make_ratios(infile, run_z_w_only=False, run_qcd_only=False, variable='mjj'):
                         unc_tag = re.findall('unc_.*', name)[0]
                     print(name)
                     # Ratio: Z(mumu) / W(munu)
-                    ratio_mu = f.Get(name).Clone(f'zmumu_over_wmunu_{unc_tag}')
+                    ratio_mu = f.Get(name).Clone(f'zmumu_over_wmunu_{source}_{variable}_{unc_tag}')
                     ratio_mu.Divide(denominator_wmunu)
                     ratio_mu.SetDirectory(of)
                     ratio_mu.Write()
-                elif name.startswith(f'zmumu_{source}') and 'zoverg' in name: 
+                if name.startswith(f'zmumu_{source}') and ('zoverg' in name or 'nominal' in name): 
                     if not f"{year}" in name or 'ewkcorr' in name:
                         continue
                     if 'nominal' in name:
@@ -220,11 +213,11 @@ def make_ratios(infile, run_z_w_only=False, run_qcd_only=False, variable='mjj'):
                         unc_tag = re.findall('unc_.*', name)[0]
                     print(name)
                     # Ratio: Z(mumu) / GJets
-                    ratio_gjets = f.Get(name).Clone(f'zmumu_over_gjets_{unc_tag}')
+                    ratio_gjets = f.Get(name).Clone(f'zmumu_over_gjets_{source}_{variable}_{unc_tag}')
                     ratio_gjets.Divide(denominator_gjets)
                     ratio_gjets.SetDirectory(of)
                     ratio_gjets.Write()
-                elif name.startswith(f'zee_{source}') and 'zoverw' in name:
+                if name.startswith(f'zee_{source}') and ('zoverw' in name or 'nominal' in name):
                     if not f"{year}" in name or 'ewkcorr' in name:
                         continue
                     if 'nominal' in name:
@@ -232,11 +225,11 @@ def make_ratios(infile, run_z_w_only=False, run_qcd_only=False, variable='mjj'):
                     else:
                         unc_tag = re.findall('unc_.*', name)[0]
                     # Ratio: Z(ee) / W(enu)
-                    ratio_e = f.Get(name).Clone(f'zee_over_wenu_{unc_tag}')
+                    ratio_e = f.Get(name).Clone(f'zee_over_wenu_{source}_{variable}_{unc_tag}')
                     ratio_e.Divide(denominator_wenu)
                     ratio_e.SetDirectory(of)
                     ratio_e.Write()
-                elif name.startswith(f'zee_{source}') and 'zoverg' in name:
+                if name.startswith(f'zee_{source}') and ('zoverg' in name or 'nominal' in name): 
                     if not f"{year}" in name or 'ewkcorr' in name:
                         continue
                     if 'nominal' in name:
@@ -244,13 +237,13 @@ def make_ratios(infile, run_z_w_only=False, run_qcd_only=False, variable='mjj'):
                     else:
                         unc_tag = re.findall('unc_.*', name)[0]
                     # Ratio: Z(ee) / GJets
-                    ratio_gjets = f.Get(name).Clone(f'zee_over_gjets_{unc_tag}')
+                    ratio_gjets = f.Get(name).Clone(f'zee_over_gjets_{source}_{variable}_{unc_tag}')
                     ratio_gjets.Divide(denominator_gjets)
                     ratio_gjets.SetDirectory(of)
                     ratio_gjets.Write()
                 
                 # Calculate W(munu) / GJets ratios
-                elif name.startswith(f'wmunu_{source}') and 'woverg' in name:
+                if name.startswith(f'wmunu_{source}') and ('woverg' in name or 'nominal' in name):
                     if not f"{year}" in name or 'ewkcorr' in name:
                         continue
                     if 'nominal' in name:
@@ -259,12 +252,12 @@ def make_ratios(infile, run_z_w_only=False, run_qcd_only=False, variable='mjj'):
                         unc_tag = re.findall('unc_.*', name)[0]
                     print(name)
                     # Ratio: W(munu) / GJets
-                    ratio_gjets = f.Get(name).Clone(f'wmunu_over_gjets_{unc_tag}')
+                    ratio_gjets = f.Get(name).Clone(f'wmunu_over_gjets_{source}_{variable}_{unc_tag}')
                     ratio_gjets.Divide(denominator_gjets)
                     ratio_gjets.SetDirectory(of)
                     ratio_gjets.Write()
                 # Calculate W(enu) / GJets ratios
-                elif name.startswith(f'wenu_{source}') and 'woverg' in name:
+                if name.startswith(f'wenu_{source}') and ('woverg' in name or 'nominal' in name):
                     if not f"{year}" in name or 'ewkcorr' in name:
                         continue
                     if 'nominal' in name:
@@ -273,7 +266,7 @@ def make_ratios(infile, run_z_w_only=False, run_qcd_only=False, variable='mjj'):
                         unc_tag = re.findall('unc_.*', name)[0]
                     print(name)
                     # Ratio: W(enu) / GJets
-                    ratio_gjets = f.Get(name).Clone(f'wenu_over_gjets_{unc_tag}')
+                    ratio_gjets = f.Get(name).Clone(f'wenu_over_gjets_{source}_{variable}_{unc_tag}')
                     ratio_gjets.Divide(denominator_gjets)
                     ratio_gjets.SetDirectory(of)
                     ratio_gjets.Write()
@@ -293,15 +286,15 @@ def make_uncertainties(infile, run_z_w_only=False, run_qcd_only=False, variable=
     for source in sources:
         for year in [2017,2018]:
             # Get the nominal ratios for each process ratio
-            zee_over_wenu_nominal = f.Get(f'zee_over_wenu_nominal_{year}')
-            zmumu_over_wmunu_nominal = f.Get(f'zmumu_over_wmunu_nominal_{year}')
-            zmumu_over_gjets_nominal = f.Get(f'zmumu_over_gjets_nominal_{year}')
-            zee_over_gjets_nominal = f.Get(f'zee_over_gjets_nominal_{year}')
-            wmunu_over_gjets_nominal = f.Get(f'wmunu_over_gjets_nominal_{year}')
-            wenu_over_gjets_nominal = f.Get(f'wenu_over_gjets_nominal_{year}')
+            zee_over_wenu_nominal = f.Get(f'zee_over_wenu_{source}_{variable}_nominal_{year}')
+            zmumu_over_wmunu_nominal = f.Get(f'zmumu_over_wmunu_{source}_{variable}_nominal_{year}')
+            zmumu_over_gjets_nominal = f.Get(f'zmumu_over_gjets_{source}_{variable}_nominal_{year}')
+            zee_over_gjets_nominal = f.Get(f'zee_over_gjets_{source}_{variable}_nominal_{year}')
+            wmunu_over_gjets_nominal = f.Get(f'wmunu_over_gjets_{source}_{variable}_nominal_{year}')
+            wenu_over_gjets_nominal = f.Get(f'wenu_over_gjets_{source}_{variable}_nominal_{year}')
             for name in map(lambda x:x.GetName(), f.GetListOfKeys()):
                 # Uncertainties for Z(mumu) / W(munu)
-                if re.match(f'.*zmumu_over_wmunu_unc_(.*)_{year}', name):
+                if re.match(f'.*zmumu_over_wmunu_{source}_{variable}_unc_(.*)_{year}', name):
                     zmumu_over_wmunu_varied = f.Get(name)
                     variation = zmumu_over_wmunu_varied.Clone(f'uncertainty_{name}')
 
@@ -314,7 +307,7 @@ def make_uncertainties(infile, run_z_w_only=False, run_qcd_only=False, variable=
                     zmumu_over_wmunu_varied.Write()
 
                 # Uncertainties for Z(ee) / W(enu)
-                elif re.match(f'.*zee_over_wenu_unc_(.*)_{year}', name):
+                elif re.match(f'.*zee_over_wenu_{source}_{variable}_unc_(.*)_{year}', name):
                     zee_over_wenu_varied = f.Get(name)
                     variation = zee_over_wenu_varied.Clone(f'uncertainty_{name}')
 
@@ -327,7 +320,7 @@ def make_uncertainties(infile, run_z_w_only=False, run_qcd_only=False, variable=
                     zee_over_wenu_varied.Write()
     
                 # Uncertainties for Z(mumu) / GJets
-                elif re.match(f'.*zmumu_over_gjets_unc_(.*)_{year}', name):
+                elif re.match(f'.*zmumu_over_gjets_{source}_{variable}_unc_(.*)_{year}', name):
                     zmumu_over_gjets_varied = f.Get(name)
                     variation = zmumu_over_gjets_varied.Clone(f'uncertainty_{name}')
 
@@ -340,7 +333,7 @@ def make_uncertainties(infile, run_z_w_only=False, run_qcd_only=False, variable=
                     zmumu_over_gjets_varied.Write()
     
                 # Uncertainties for Z(ee) / GJets
-                elif re.match(f'.*zee_over_gjets_unc_(.*)_{year}', name):
+                elif re.match(f'.*zee_over_gjets_{source}_{variable}_unc_(.*)_{year}', name):
                     zee_over_gjets_varied = f.Get(name)
                     variation = zee_over_gjets_varied.Clone(f'uncertainty_{name}')
 
@@ -353,7 +346,7 @@ def make_uncertainties(infile, run_z_w_only=False, run_qcd_only=False, variable=
                     zee_over_gjets_varied.Write()
     
                 # Uncertainties for W(munu) / GJets
-                elif re.match(f'.*wmunu_over_gjets_unc_(.*)_{year}', name):
+                elif re.match(f'.*wmunu_over_gjets_{source}_{variable}_unc_(.*)_{year}', name):
                     wmunu_over_gjets_varied = f.Get(name)
                     variation = wmunu_over_gjets_varied.Clone(f'uncertainty_{name}')
 
@@ -366,7 +359,7 @@ def make_uncertainties(infile, run_z_w_only=False, run_qcd_only=False, variable=
                     wmunu_over_gjets_varied.Write()
     
                 # Uncertainties for W(enu) / GJets
-                elif re.match(f'.*wenu_over_gjets_unc_(.*)_{year}', name):
+                elif re.match(f'.*wenu_over_gjets_{source}_{variable}_unc_(.*)_{year}', name):
                     wenu_over_gjets_varied = f.Get(name)
                     variation = wenu_over_gjets_varied.Clone(f'uncertainty_{name}')
 
@@ -386,31 +379,39 @@ def combine_uncs(infile, variable):
     f = r.TFile(infile)
     of = r.TFile(infile.replace('_ratio_unc', '_ratio_unc_combined'), 'RECREATE')
 
-    sources = ['qcd']
+    sources = ['qcd', 'ewk']
 
     for source in sources:
         for year in [2017,2018]:
+            # unc_sources = [
+                # f'unc_zoverw_nlo_muf_down_{year}',
+                # f'unc_zoverw_nlo_muf_up_{year}',
+                # f'unc_zoverw_nlo_mur_down_{year}',
+                # f'unc_zoverw_nlo_mur_up_{year}',
+                # f'unc_zoverw_nlo_pdf_down_{year}',
+                # f'unc_zoverw_nlo_pdf_up_{year}'
+            # ]
             unc_sources = [
-                f'unc_zoverw_nlo_muf_down_{year}',
-                f'unc_zoverw_nlo_muf_up_{year}',
-                f'unc_zoverw_nlo_mur_down_{year}',
-                f'unc_zoverw_nlo_mur_up_{year}',
-                f'unc_zoverw_nlo_pdf_down_{year}',
-                f'unc_zoverw_nlo_pdf_up_{year}'
+                f'nlo_muf_down_{year}',
+                f'nlo_muf_up_{year}',
+                f'nlo_mur_down_{year}',
+                f'nlo_mur_up_{year}',
+                f'nlo_pdf_down_{year}',
+                f'nlo_pdf_up_{year}'
             ]
             # Read the uncertainties for Z(ee)/W(enu) and Z(mumu)/W(munu)
             # Calculate the combined uncertainties on Z(ll)/W(lnu)
             for unc_source in unc_sources:
                 print(unc_source)
-                unc_zmumu_over_wmunu = f.Get(f'uncertainty_zmumu_over_wmunu_{unc_source}')
-                unc_zee_over_wenu = f.Get(f'uncertainty_zee_over_wenu_{unc_source}')
-                unc_zmumu_over_gjets = f.Get(f'uncertainty_zmumu_over_gjets_{unc_source}')
-                unc_zee_over_gjets = f.Get(f'uncertainty_zee_over_gjets_{unc_source}')
-                unc_wmunu_over_gjets = f.Get(f'uncertainty_wmunu_over_gjets_{unc_source}')
-                unc_wenu_over_gjets = f.Get(f'uncertainty_wenu_over_gjets_{unc_source}')
+                unc_zmumu_over_wmunu = f.Get(f'uncertainty_zmumu_over_wmunu_{source}_{variable}_unc_zoverw_{unc_source}')
+                unc_zee_over_wenu = f.Get(f'uncertainty_zee_over_wenu_{source}_{variable}_unc_zoverw_{unc_source}')
+                unc_zmumu_over_gjets = f.Get(f'uncertainty_zmumu_over_gjets_{source}_{variable}_unc_zoverg_{unc_source}')
+                unc_zee_over_gjets = f.Get(f'uncertainty_zee_over_gjets_{source}_{variable}_unc_zoverg_{unc_source}')
+                unc_wmunu_over_gjets = f.Get(f'uncertainty_wmunu_over_gjets_{source}_{variable}_unc_woverg_{unc_source}')
+                unc_wenu_over_gjets = f.Get(f'uncertainty_wenu_over_gjets_{source}_{variable}_unc_woverg_{unc_source}')
 
                 # Combine in quadrature: Z / W
-                combined_unc_zll_over_wlnu = f.Get(f'uncertainty_zmumu_over_wmunu_{unc_source}').Clone(f'uncertainty_zll_over_wlnu_{unc_source}')
+                combined_unc_zll_over_wlnu = f.Get(f'uncertainty_zmumu_over_wmunu_{source}_{variable}_unc_zoverw_{unc_source}').Clone(f'uncertainty_zll_over_wlnu_{source}_{variable}_unc_zoverw_{unc_source}')
                 for idx in range(1, unc_zmumu_over_wmunu.GetNbinsX()+1):
                     zmumu_over_wmunu_unc = unc_zmumu_over_wmunu.GetBinContent(idx)
                     zee_over_wenu_unc = unc_zee_over_wenu.GetBinContent(idx)
@@ -420,7 +421,7 @@ def combine_uncs(infile, variable):
                         combined_unc_zll_over_wlnu.SetBinContent(idx, 1-np.sqrt( (1-zmumu_over_wmunu_unc)**2 + (1-zee_over_wenu_unc)**2))
 
                 # Combine in quadrature: Z / gamma
-                combined_unc_zll_over_gjets = f.Get(f'uncertainty_zmumu_over_gjets_{unc_source}').Clone(f'uncertainty_zll_over_gjets_{unc_source}')
+                combined_unc_zll_over_gjets = f.Get(f'uncertainty_zmumu_over_gjets_{source}_{variable}_unc_zoverg_{unc_source}').Clone(f'uncertainty_zll_over_gjets_{source}_{variable}_unc_zoverg_{unc_source}')
                 for idx in range(1, unc_zmumu_over_gjets.GetNbinsX()+1):
                     zmumu_over_gjets_unc = unc_zmumu_over_gjets.GetBinContent(idx)
                     zee_over_gjets_unc = unc_zee_over_gjets.GetBinContent(idx)
@@ -430,19 +431,22 @@ def combine_uncs(infile, variable):
                         combined_unc_zll_over_gjets.SetBinContent(idx, 1-np.sqrt( (1-zmumu_over_gjets_unc)**2 + (1-zee_over_gjets_unc)**2))
                             
                 # Combine in quadrature: W / gamma
-                # combined_unc_wlnu_over_gjets = f.Get(f'uncertainty_wmunu_over_gjets_{unc_source}').Clone()
-                # for idx in range(1, unc_zmumu_over_gjets.GetNbinsX()+1):
-                    # wmunu_over_gjets_unc = unc_wmunu_over_gjets.GetBinContent(idx)
-                    # wenu_over_gjets_unc = unc_wenu_over_gjets.GetBinContent(idx)
-                    # combined_unc_wlnu_over_gjets.SetBinContent(idx, np.sqrt( (1-wmunu_over_gjets_unc)**2 + (1-wenu_over_gjets_unc)**2))
+                combined_unc_wlnu_over_gjets = f.Get(f'uncertainty_wmunu_over_gjets_{source}_{variable}_unc_woverg_{unc_source}').Clone(f'uncertainty_wlv_over_gjets_{source}_{variable}_unc_woverg_{unc_source}')
+                for idx in range(1, unc_zmumu_over_gjets.GetNbinsX()+1):
+                    wmunu_over_gjets_unc = unc_wmunu_over_gjets.GetBinContent(idx)
+                    wenu_over_gjets_unc = unc_wenu_over_gjets.GetBinContent(idx)
+                    if 'up' in unc_source:
+                        combined_unc_wlnu_over_gjets.SetBinContent(idx, 1+np.sqrt( (1-wmunu_over_gjets_unc)**2 + (1-wenu_over_gjets_unc)**2))
+                    elif 'down' in unc_source:
+                        combined_unc_wlnu_over_gjets.SetBinContent(idx, 1-np.sqrt( (1-wmunu_over_gjets_unc)**2 + (1-wenu_over_gjets_unc)**2))
                             
                 # Save all histograms into the output ROOT file
                 combined_unc_zll_over_wlnu.SetDirectory(of)
                 combined_unc_zll_over_wlnu.Write()
                 combined_unc_zll_over_gjets.SetDirectory(of)
                 combined_unc_zll_over_gjets.Write()
-                # combined_unc_wlnu_over_gjets.SetDirectory(of)
-                # combined_unc_wlnu_over_gjets.Write()
+                combined_unc_wlnu_over_gjets.SetDirectory(of)
+                combined_unc_wlnu_over_gjets.Write()
 
 import os
 pjoin = os.path.join
