cdd979596461823016a853bddd73621eb608a448
Alp Akpinar <aakpinar@bu.edu>
2020-08-20 10:56:54 -0400

    Add code to produce data/MC comparison plots

--- bucoffea/plot/studies/fraction_cut_study/plot_comp.py
+++ bucoffea/plot/studies/fraction_cut_study/plot_comp.py
@@ -23,6 +23,21 @@ XLABELS = {
     'met' : r'$p_T^{miss}$ (GeV)'
 }
 
+def preprocess(h, acc, variable):
+    '''Preprocessing for histograms: scaling + rebinning.'''
+    h = merge_extensions(h, acc, reweight_pu=False)
+    scale_xs_lumi(h)
+    h = merge_datasets(h)
+
+    # Rebin, if neccessary
+    if variable in REBIN.keys():
+        if variable == 'ak4_pt0':
+            h = h.rebin('jetpt', REBIN[variable])
+        elif variable == 'met':
+            h = h.rebin('met', REBIN[variable])
+    
+    return h
+
 def make_plot(h, outtag, mode='data', variable='ak4_pt0'):
     '''Make the comparison plot for data or MC and save it.'''
     fig, (ax, rax) = plt.subplots(2, 1, figsize=(7,7), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
@@ -68,18 +83,7 @@ def plot_comparison(acc, outtag, variable='ak4_pt0'):
     '''Plot spectrum for the given variable, with and without the EM fraction cut.'''
     acc.load(variable)
     h = acc[variable]
-
-    h = merge_extensions(h, acc, reweight_pu=False)
-    scale_xs_lumi(h)
-    h = merge_datasets(h)
-
-    # Rebin, if neccessary
-    if variable in REBIN.keys():
-        if variable == 'ak4_pt0':
-            h = h.rebin('jetpt', REBIN[variable])
-        elif variable == 'met':
-            h = h.rebin('met', REBIN[variable])
-    
+    h = preprocess(h, acc, variable)
 
     # Get data and MC (EGamma)
     h_data = h.integrate('dataset', 'EGamma_2017')[re.compile('.*EmEF.*')]
@@ -88,6 +92,57 @@ def plot_comparison(acc, outtag, variable='ak4_pt0'):
     # Make the plots for data and MC
     make_plot(h_data, outtag, mode='data', variable=variable)
     make_plot(h_mc, outtag, mode='mc', variable=variable)
+
+def plot_data_mc_comparison(acc, outtag, variable='ak4_pt0'):
+    '''Plot data/MC comparison for the given variable, without the EM fraction cut.'''
+    acc.load(variable)
+    h = acc[variable]
+    h = preprocess(h, acc, variable)
+
+    # Get the region without EM fraction cut applied
+    h = h.integrate('region', 'cr_g_noEmEF')
+
+    # Plot the two histograms, and their ratio
+    fig, (ax, rax) = plt.subplots(2, 1, figsize=(7,7), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
+    hist.plot1d(h, ax=ax, overlay='dataset')
+
+    ax.set_xlabel('')
+    ax.set_yscale('log')
+    ax.set_ylim(1e-1, 1e8)
+
+    data_err_opts = {
+        'linestyle':'none',
+        'marker': '.',
+        'markersize': 10.,
+        'color':'k'
+    }
+    h_num = h.integrate('dataset', 'EGamma_2017')
+    h_denom = h.integrate('dataset', re.compile('GJets_DR-0p4.*2017'))
+    hist.plotratio(h_num, h_denom, ax=rax, error_opts=data_err_opts, unc='num')
+
+    rax.set_xlabel(XLABELS[variable])
+    rax.set_ylabel('Data / MC')
+    rax.set_ylim(0.8,1.2)
+    rax.grid(True)
+
+    if variable == 'ak4_nef0':
+        ylim = rax.get_ylim()
+        rax.plot([0.7, 0.7], ylim, color='red')
+        rax.set_ylim(ylim)
+
+    xlim = rax.get_xlim()
+    rax.plot(xlim, [1, 1], color='red')
+    rax.set_xlim(xlim)
+
+    # Save the figure
+    outdir = f'./output/{outtag}'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    
+    outname = f'data_mc_comp_{variable}.pdf'
+    outpath = pjoin(outdir, outname)
+    fig.savefig(outpath)
+    print(f'File saved: {outpath}')
 
 def main():
     inpath = sys.argv[1]
@@ -107,10 +162,11 @@ def main():
         outtag = inpath.split('/')[-1]
 
     # Variables to plot
-    variables = ['ak4_pt0', 'ak4_eta0', 'ak4_nef0', 'met']
+    variables = ['ak4_pt0', 'ak4_eta0', 'ak4_nef0']
 
     for variable in variables:
         plot_comparison(acc, outtag, variable=variable)
+        plot_data_mc_comparison(acc, outtag, variable=variable)
 
 if __name__ == '__main__':
     main()
