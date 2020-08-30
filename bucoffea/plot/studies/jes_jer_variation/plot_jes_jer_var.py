Fixes to JER calculation

bucoffea/plot/studies/jes_jer_variation/plot_jes_jer_var.py
@@ -319,7 +319,7 @@ def plot_jes_jer_var_ratio(acc, regex1, regex2, region1, region2, tag, out_tag, 
         else:
             # Symmetric JER up/down variations
             ratios['_jerUp'] = h1_sumw / h2_sumw
-            ratios['_jerDown'] = 1 - h1_sumw / h2_sumw
+            ratios['_jerDown'] = ratios[''] - (ratios['_jerUp'] - ratios[''])
         
         # Gaussian error propagation
         gaus_error = np.sqrt((h2_sumw*np.sqrt(h1_sumw2))**2 + (h1_sumw*np.sqrt(h2_sumw2))**2)/h2_sumw**2

bucoffea/plot/studies/jes_jer_variation/plot_split_jes.py
@@ -166,6 +166,8 @@ def plot_split_jecunc(acc, out_tag, dataset_tag, year, binnings, plot_total=True
 
                 ratio_jerUp[np.isnan(ratio_jerUp) | np.isinf(ratio_jerUp)] = 1.
                 ratio_jerDown[np.isnan(ratio_jerDown) | np.isinf(ratio_jerDown)] = 1.
+                
+                edges = h_nom.axis(variable_to_use).edges()
 
                 rootfile[f'{dataset_tag}_jerUp'] = (ratio_jerUp, edges)
                 rootfile[f'{dataset_tag}_jerDown'] = (ratio_jerDown, edges)
@@ -487,9 +489,10 @@ def main():
 
     for dataset_tag in dataset_tags:
         # If specified, run only on specified processes, skip otherwise
-        if not re.match(args.onlyRun, dataset_tag):
-            print(f'MSG% Skipping: {dataset_tag}')
-            continue
+        if args.onlyRun:
+            if not re.match(args.onlyRun, dataset_tag):
+                print(f'MSG% Skipping: {dataset_tag}')
+                continue
 
         print(f'MSG% Working on: {dataset_tag}')
         # Determine the year of the dataset

bucoffea/plot/studies/jes_jer_variation/plot_split_jes_ratio.py
@@ -125,7 +125,7 @@ def plot_split_jecunc_ratios(acc, out_tag, transfer_factor_tag, dataset_info, ye
     
     # Setup the color map
     colormap = plt.cm.nipy_spectral
-    num_plots = len(vars_to_look_at) if skimmed else 12
+    num_plots = len(vars_to_look_at) if skimmed else 13 # 12 JES variations + 1 JER variation
     colors = []
     for i in np.linspace(0,0.9,num_plots):
         colors.append([colormap(i), colormap(i)])
@@ -170,7 +170,7 @@ def plot_split_jecunc_ratios(acc, out_tag, transfer_factor_tag, dataset_info, ye
 
         else:
             varied_ratio_jerUp = h_varied_num.values()[()] / h_varied_den.values()[()]
-            varied_ratio_jerDown = 2 - varied_ratio_jerUp
+            varied_ratio_jerDown = nominal_ratio - (varied_ratio_jerUp - nominal_ratio)
 
             dratio_jerUp = varied_ratio_jerUp / nominal_ratio
             dratio_jerDown = varied_ratio_jerDown / nominal_ratio