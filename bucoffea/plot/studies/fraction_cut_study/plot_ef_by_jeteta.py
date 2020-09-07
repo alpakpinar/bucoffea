3ba11ff495371721e1c279a5cead35afeda88c91
Alp Akpinar <aakpinar@bu.edu>
2020-09-07 17:12:24 -0400

    Aesthetic changes

--- bucoffea/plot/studies/fraction_cut_study/plot_ef_by_jeteta.py
+++ bucoffea/plot/studies/fraction_cut_study/plot_ef_by_jeteta.py
@@ -38,29 +38,57 @@ def plot_ef_by_jeteta(acc, outtag, year):
     for region in regions:
         region_tag = re.findall('jeteta_.*', region)[0]
         print(f'Region tag: {region_tag}')
-        histo = h[re.compile(f'.*{region_tag}.*')]
+        histo = h.integrate('region', region)[re.compile(f'.*{year}')]
+
+        fig_titles = {
+            'jeteta_lt_2_3' : r'$|\eta| < 2.3$', 
+            'jeteta_jet_eta_gt_2_3_lt_2_7' : r'$2.3 < |\eta| < 2.7$', 
+            'jeteta_jet_eta_gt_2_7_lt_3_0' : r'$2.7 < |\eta| < 3.0$', 
+            'jeteta_gt_3_0' : r'$|\eta| > 3.0$'
+        }
 
         fig, (ax, rax) = plt.subplots(2, 1, figsize=(7,7), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
         hist.plot1d(histo, ax=ax, overlay='dataset')
         ax.set_xlabel('')
         ax.set_yscale('log')
         ax.set_ylim(1e-1,1e6)
+        ax.set_title(fig_titles[region_tag])
+
+        ylim = ax.get_ylim()
+        ax.plot([0.7, 0.7], ylim, color='red')
+        ax.set_ylim(ylim)
 
         # Plot data/MC ratio
-        h_data = histo.integrate('dataset', f'MET_{year}')
+        data_err_opts = {
+            'linestyle':'none',
+            'marker': '.',
+            'markersize': 10.,
+            'color':'k'
+        }
+
+        h_data = histo.integrate('dataset', f'SingleMuon_{year}')
         h_mc = histo.integrate('dataset', re.compile(f'DYJetsToLL.*{year}'))
-        hist.plotratio(h_data, h_mc, unc='num', ax=rax)
+        hist.plotratio(h_data, h_mc, unc='num', ax=rax, error_opts=data_err_opts)
         
         rax.set_xlabel('Jet neutral EM fraction')
+        rax.set_ylabel('Data / MC')
         rax.grid(True)
         rax.set_ylim(0.5,1.5)
+
+        xlim = rax.get_xlim()
+        rax.plot(xlim, [1, 1], color='red')
+        rax.set_xlim(xlim)
+
+        ylim = rax.get_ylim()
+        rax.plot([0.7, 0.7], ylim, color='red')
+        rax.set_ylim(ylim)
 
         # Save figure
         outdir = f'./output/{outtag}'
         if not os.path.exists(outdir):
             os.makedirs(outdir)
         
-        outpath = pjoin(outdir, f'{region_tag}_data_mc.pdf')
+        outpath = pjoin(outdir, f'{region_tag}_data_mc_{year}.pdf')
         fig.savefig(outpath)
 
         print(f'File saved: {outpath}')
@@ -73,7 +101,7 @@ def main():
     acc.load('sumw')
     acc.load('sumw2')
 
-    outtag = re.findall('^merged_.*', inpath)[0].replace('/','')
+    outtag = re.findall('merged_.*', inpath)[0].replace('/','')
 
     for year in [2017, 2018]:
         plot_ef_by_jeteta(acc, outtag, year=year)
