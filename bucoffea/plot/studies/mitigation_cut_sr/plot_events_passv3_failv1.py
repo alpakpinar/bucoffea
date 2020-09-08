379334fae238b618b695fae4aaa78f9c777168a0
Alp Akpinar <aakpinar@bu.edu>
2020-09-08 17:04:10 -0400

    Add function to plot events where jets have very low or very high energy fractions

--- bucoffea/plot/studies/mitigation_cut_sr/plot_events_passv3_failv1.py
+++ bucoffea/plot/studies/mitigation_cut_sr/plot_events_passv3_failv1.py
@@ -4,6 +4,7 @@ import os
 import sys
 import re
 import warnings
+import argparse
 from bucoffea.plot.util import merge_datasets, merge_extensions, scale_xs_lumi
 from coffea import hist
 from matplotlib import pyplot as plt
@@ -24,6 +25,13 @@ XLABELS = {
     'ak4_nhf0' : r'Leading jet neutral hadron fraction',
     'ak4_nhf1' : r'Trailing jet neutral hadron fraction'
 }
+
+def parse_cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inpath', help='Path containing merged coffea files.')
+    parser.add_argument('--plot_spec', help='Plot distributions for events with specific jets, with very low/high fractions.', action='store_true')
+    args = parser.parse_args()
+    return args
 
 def plot_events(acc, outtag, variable):
     '''Plot events which pass the v3 selection but fail the v1 selection.'''
@@ -60,8 +68,44 @@ def plot_events(acc, outtag, variable):
     fig.savefig(outpath)
     print(f'File saved: {outpath}')
 
+def plot_events_specific_jets(acc, outtag, variable, spec='low_EmEF'):
+    '''Plot events which pass the v3 selection but fail the v1 selection.
+    Leading jet in these events also has either very low or very high fractions.'''
+    acc.load(variable)
+    h = acc[variable]
+
+    h = merge_extensions(h, acc, reweight_pu=False)
+    scale_xs_lumi(h)
+    h = merge_datasets(h)
+
+    # Get the relevant region
+    h = h.integrate('region', f'sr_vbf_passv3_failv1_{spec}').integrate('dataset', 'MET_2017')
+
+    spec_to_title = {
+        'low_EmEF' : r'$NeEmEF < 0.1$',
+        'high_EmEF' : r'$NeEmEF > 0.9$',
+        'low_HEF' : r'$NeHEF < 0.1$',
+        'high_HEF' : r'$NeHEF > 0.9$'
+    }
+
+    fig, ax = plt.subplots()
+    hist.plot1d(h, ax=ax)
+    ax.get_legend().remove()
+    ax.set_title(f'Events passing EEv3, failing EEv1 {spec_to_title[spec]}')
+    ax.set_xlabel(XLABELS[variable])
+
+    # Save figure
+    outdir = f'./output/{outtag}/{spec}'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    
+    outpath = pjoin(outdir, f'{variable}.pdf')
+    fig.savefig(outpath)
+    print(f'File saved: {outpath}')
+
 def main():
-    inpath = sys.argv[1]
+    args = parse_cli()
+    inpath = args.inpath
     acc = dir_archive(inpath)
 
     acc.load('sumw')
@@ -72,6 +116,13 @@ def main():
     variables = ['ak4_eta0', 'ak4_eta1', 'ak4_phi0', 'ak4_phi1', 'ak4_nef0', 'ak4_nef1', 'ak4_nhf0', 'ak4_nhf1']
     for variable in variables:
         plot_events(acc, outtag, variable=variable)
+
+        # If requested, plot events where jets have either very low or very high energy fractions
+        # (Specified by "spec": e.g. low_EmEF refers to jets with neutral EM energy fraction < 0.1)
+        if args.plot_spec:
+            specs = ['low_EmEF', 'high_EmEF', 'low_HEF', 'high_HEF']
+            for spec in specs:
+                plot_events_specific_jets(acc, outtag, variable, spec=spec)
 
 if __name__ == '__main__':
     main()
