f43cc93d1e0dff23f5c49e5adeb0c024d271c530
Alp Akpinar <aakpinar@bu.edu>
2020-09-20 20:07:31 -0400

    Add better labeled names for each cut to use in cutflow printing

--- bucoffea/plot/studies/compare_with_ic/dump_cutflow.py
+++ bucoffea/plot/studies/compare_with_ic/dump_cutflow.py
@@ -10,6 +10,34 @@ from pprint import pprint
 
 pjoin = os.path.join
 
+pretty_names_for_cuts = {
+    'all' : 'All Events',
+    'trig_met' : 'MET trigger requirement',
+    'metphihemextveto' : 'MET phi HEM veto for 2018',
+    'hornveto' : 'Jet horn veto',
+    'veto_ele' : 'Electron veto',
+    'veto_muo' : 'Muon veto',
+    'filt_met' : 'MET cleaning filters',
+    'mindphijr' : 'min(Delta phi jet MET) > 0.5',
+    'recoil' : 'Recoil > 250 GeV',
+    'two_jets' : 'Exactly two jets requirement',
+    'leadak4_pt_eta' : 'Leading jet pt&eta cut',
+    'leadak4_id' : 'Leading jet ID cut',
+    'trailak4_pt_eta' : 'Trailing jet pt&eta cut',
+    'trailak4_id' : 'Trailing jet ID cut',
+    'hemisphere' : 'Opposite hemispheres for two leading jets',
+    'mjj' : 'mjj cut',
+    'dphijj' : 'dphijj cut',
+    'detajj' : 'detajj cut',
+    'veto_photon' : 'Photon veto',
+    'veto_tau' : 'Tau veto',
+    'veto_b' : 'b-jet veto',
+    'dpfcalo_sr' : '(PF MET - Calo MET) cut',
+    'eemitigation' : 'VecB/VecDPhi cuts',
+    'max_neEmEF' : 'Jet EM energy fracion cut',
+    'veto_hfhf' : 'HF-HF veto'
+}
+
 def dump_cutflow(acc, dataset):
     '''Dump the cutflow for the signal region cuts out of the given accumulator, into an output txt file'''
     acc.load('cutflow_sr_vbf')
@@ -18,7 +46,7 @@ def dump_cutflow(acc, dataset):
     # Tabulate the cutflow
     table = []
     for cut, count in sorted(cf.items(), key=lambda x:x[1], reverse=True):
-        table.append([cut, count])
+        table.append([pretty_names_for_cuts[cut], count])
 
     text = tabulate.tabulate(table, headers=["Cut", "Passing events"], floatfmt=".1f")
 
