b0fbdf7f60acb186f32aa9f88f1dd4527e0da6b0
Alp Akpinar <aakpinar@bu.edu>
2020-11-01 11:47:15 -0500

    Compute poisson error on the ratios, add title

--- bucoffea/plot/studies/jes_jer_variation/compare_shapes_old_vs_new_jes.py
+++ bucoffea/plot/studies/jes_jer_variation/compare_shapes_old_vs_new_jes.py
@@ -11,6 +11,7 @@ from bucoffea.helpers.paths import bucoffea_path
 from coffea import hist
 from klepto.archives import dir_archive
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MultipleLocator
 from pprint import pprint
 
 pjoin = os.path.join
@@ -34,6 +35,16 @@ def preprocess(h,acc):
     h = h.rebin('mjj', mjj_bin)
 
     return h
+
+def calculate_poisson_err(rsumw, sumw2_var, sumw_nom):
+    '''
+    Calculate the Poisson error on varied / nominal ratio.
+    Returns an interval containing the up / down variations on errors.
+    '''
+    err = np.abs(
+        hist.poisson_interval(rsumw, sumw2_var / sumw_nom**2) - rsumw
+    )
+    return err
 
 def integrate_over_dataset(h, year, dataset_tag):
     '''For a given histogram, integrate over the relevant dataset and region and return the new histogram.'''
@@ -64,51 +75,69 @@ def make_comparison_plot(acc_dict, dataset_tag, year, variation='jesRelativeBal'
     centers = histos['v1'].axis('mjj').centers()
 
     # Now, calculate the nominal yields and the variations for the two versions
-    nom_yields = {}
-    var_yields = {}
+    nom_sumw, nom_sumw2 = {}, {}
+    var_sumw, var_sumw2 = {}, {}
     for v, h in histos.items():
         # Nominal yields: Just the regular SR
-        nom_yields[v] = h.integrate('region', 'sr_vbf').values()[()]
-        var_yields[v] = {}
+        nom_sumw[v], nom_sumw2[v] = h.integrate('region', 'sr_vbf').values(sumw2=True)[()]
+        var_sumw[v], var_sumw2[v] = {}, {}
         for region in h.identifiers('region'):
             if variation not in region.name:
                 continue
+            print(f'Region: {region.name}')
+            print(f'Variation: {variation}')
             # Compute and store the up/down variations for this specific variation
             var_direction = re.findall('Up|Down', region.name)[0]
-            var_yields[v][var_direction] = h.integrate('region', region).values()[()]
+            print(f'Variation direction: {var_direction}')
+            var_sumw[v][var_direction], var_sumw2[v][var_direction] = h.integrate('region', region).values(sumw2=True)[()]
 
     # Compute the varied / nominal ratios and plot them
     fig, ax, rax = fig_ratio()
-    ratios_up = {}
-    ratios_down = {}
+    ratios_up, err_up = {}, {}
+    ratios_down, err_down = {}, {}
     for v in histos.keys():
-        sumw_nom  = nom_yields[v]
-        sumw_up   = var_yields[v]['Up']
-        sumw_down = var_yields[v]['Down']
+        sumw_nom  = nom_sumw[v]
+        sumw_up   = var_sumw[v]['Up']
+        sumw_down = var_sumw[v]['Down']
         
         ratios_up[v]   = sumw_up / sumw_nom
         ratios_down[v] = sumw_down / sumw_nom
 
-        hep.histplot(ratios_up[v], edges, ax=ax, label=f'Up {v}', histtype='errorbar')
-        hep.histplot(ratios_down[v], edges, ax=ax, label=f'Down {v}', histtype='errorbar')
+        # Calculate the errors in the ratios
+        err_up[v] = calculate_poisson_err(ratios_up[v], var_sumw2[v]['Up'], sumw_nom)
+        err_down[v] = calculate_poisson_err(ratios_down[v], var_sumw2[v]['Down'], sumw_nom)
+
+        hep.histplot(ratios_up[v], edges, yerr=err_up[v], ax=ax, label=f'Up {v}', histtype='errorbar')
+        hep.histplot(ratios_down[v], edges, yerr=err_down[v], ax=ax, label=f'Down {v}', histtype='errorbar')
 
     ax.set_ylabel('Variation / Nominal')
     ax.set_ylim(0.6,1.4)
     ax.grid(True)
     ax.legend()
 
+    ax.set_title(f'Variation: {variation}')
+
     # Plot ratio of variations between the two versions
     dratio_up = ratios_up['v2'] / ratios_up['v1']
     dratio_down = ratios_down['v2'] / ratios_down['v1']
 
-    rax.plot(centers, dratio_up, label='Up', **data_err_opts)
-    rax.plot(centers, dratio_down, label='Down', **data_err_opts)
+    # The errors on the ratio, scaled by denominator
+    ratio_up_err = err_up['v2'] / ratios_up['v1']
+    ratio_down_err = err_down['v2'] / ratios_down['v1']
+
+    rax.errorbar(centers, dratio_up, yerr=ratio_up_err, label='Up', **data_err_opts)
+    rax.errorbar(centers, dratio_down, yerr=ratio_down_err, label='Down', **data_err_opts)
 
     rax.set_ylabel('v2 / v1')
     rax.set_xlabel(r'$M_{jj} \ (GeV)$')
     rax.grid(True)
-    rax.set_ylim(0.6,1.4)
+    rax.set_ylim(0.4,1.6)
     rax.legend(ncol=2)
+
+    loc1 = MultipleLocator(0.2)
+    loc2 = MultipleLocator(0.1)
+    rax.yaxis.set_major_locator(loc1)
+    rax.yaxis.set_minor_locator(loc2)
 
     # Save figure
     outdir = './output/jes_comparison_v1_v2'
@@ -137,7 +166,7 @@ def main():
     make_comparison_plot(acc_dict, 
             dataset_tag='qcd_zjets_2017',
             year=2017,
-            variation='jesRelativeBal'
+            variation='jesRelativeSample'
             )
             
 if __name__ == '__main__':
