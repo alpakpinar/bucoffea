9dd97fdc45c4cff8093a714c83472fd849562bd0
Alp Akpinar <aakpinar@bu.edu>
2020-11-07 13:37:02 -0500

    Aesthetic changes + run for more datasets and both years

--- bucoffea/plot/studies/jet_id_comparison/compare_shapes.py
+++ bucoffea/plot/studies/jet_id_comparison/compare_shapes.py
@@ -7,6 +7,7 @@ from coffea import hist
 from bucoffea.plot.util import merge_datasets, merge_extensions, scale_xs_lumi, fig_ratio
 from bucoffea.helpers.paths import bucoffea_path
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MultipleLocator
 from klepto.archives import dir_archive
 from pprint import pprint
 
@@ -14,8 +15,8 @@ pjoin = os.path.join
 
 def get_legend_label(key):
     mapping = {
-        'jet_id_all' : 'Jet cut applied everywhere',
-        'jet_id_on_endcap' : 'Jet cut only on endcaps'
+        'jet_id_all' : 'Jet cut: Everywhere',
+        'jet_id_on_endcap' : 'Jet cut: Endcap jets'
     }
 
     return mapping[key]
@@ -28,6 +29,17 @@ def get_dataset_regex(dataset_tag, year):
     }
     
     return mapping[dataset_tag]
+
+def get_title(dataset_tag, year):
+    mapping = {
+        'vbf' : r'VBF $H(inv)$ {}',
+        'qcd_zvv' : r'QCD $Z(\nu\nu)$ {}',
+        'ewk_zvv' : r'EWK $Z(\nu\nu)$ {}'
+    }
+
+    title = mapping[dataset_tag].format(year)
+
+    return title
 
 def preprocess(h, acc, dataset_regex, year, variable):
     h = merge_extensions(h, acc, reweight_pu=False)
@@ -64,9 +76,10 @@ def compare_shapes(acc_dict, outtag, year, dataset_tag, variable='mjj'):
         legend_labels.append( get_legend_label(key) )
 
     ax.legend(labels=legend_labels)
-    ax.set_yscale('log')
-    ax.set_ylim(1e-3,1e5)
+    # ax.set_yscale('log')
+    # ax.set_ylim(1e-3,1e5)
     ax.set_xlabel('')
+    ax.set_title( get_title(dataset_tag, year) )
 
     data_err_opts = {
         'linestyle':'none',
@@ -85,9 +98,16 @@ def compare_shapes(acc_dict, outtag, year, dataset_tag, variable='mjj'):
     )
 
     rax.set_xlabel(r'$M_{jj} \ (GeV)$')
-    rax.set_ylabel('All / Endcap')
+    rax.set_ylabel('Everywhere / Endcap')
     rax.grid(True)
-    rax.set_ylim(0.8,1.2)
+    rax.set_ylim(0.9,1.1)
+
+    loc1 = MultipleLocator(0.05)
+    loc2 = MultipleLocator(0.01)
+    rax.yaxis.set_major_locator(loc1)
+    rax.yaxis.set_minor_locator(loc2)
+
+    rax.axhline(1, xmin=0, xmax=1, color='red')
 
     # Save figure
     outdir = f'./output/{outtag}'
@@ -117,13 +137,17 @@ def main():
     # Tag for tracking the inputs 
     outtag = '06Nov20'
     
-    compare_shapes(
-        acc_dict,
-        outtag=outtag,
-        year=2017,
-        dataset_tag='vbf',
-        variable='mjj'
-    )
+    dataset_tags = ['vbf', 'qcd_zvv', 'ewk_zvv']
+    years = [2017, 2018]
+    for year in years:
+        for dataset_tag in dataset_tags:
+            compare_shapes(
+                acc_dict,
+                outtag=outtag,
+                year=year,
+                dataset_tag=dataset_tag,
+                variable='mjj'
+            )
 
 if __name__ == '__main__':
     main()
