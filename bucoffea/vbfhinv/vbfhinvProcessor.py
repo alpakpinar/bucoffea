8fed3a7805172fa3967188a54b1c0a5967587fe7
Alp Akpinar <aakpinar@bu.edu>
2020-11-10 08:40:55 -0600

    Fixup + do not save inclusive

--- bucoffea/vbfhinv/vbfhinvProcessor.py
+++ bucoffea/vbfhinv/vbfhinvProcessor.py
@@ -517,7 +517,7 @@ class vbfhinvProcessor(processor.ProcessorABC):
             mask = selection.all(*cuts)
 
             if cfg.RUN.SAVE.TREE:
-                if region in ['sr_vbf', 'sr_vbf_qcd_cr', 'sr_vbf_qcd_recoil_100_160_sr', 'sr_vbf_qcd_recoil_100_160_cr']:
+                if region in ['sr_vbf', 'sr_vbf_qcd_cr', 'sr_vbf_qcd_regionA', 'sr_vbf_qcd_regionB']:
                     output['tree_int64'][region]["event"]       +=  processor.column_accumulator(df["event"][mask])
                     output['tree_int64'][region]["run"]       +=  processor.column_accumulator(df["run"][mask])
                     output['tree_int64'][region]["luminosityBlock"]       +=  processor.column_accumulator(df["luminosityBlock"][mask])
@@ -553,11 +553,7 @@ class vbfhinvProcessor(processor.ProcessorABC):
                     for name, w in region_weights._weights.items():
                         output['tree_float16'][region][f"weight_{name}"] += processor.column_accumulator(np.float16(w[mask]))
                     output['tree_float16'][region][f"weight_total"] += processor.column_accumulator(np.float16(rweight[mask]))
-                if region=='inclusive':
-                    output['tree_int64'][region]["event"]       +=  processor.column_accumulator(df["event"][mask])
-                    for name in selection.names:
-                        output['tree_bool'][region][name] += processor.column_accumulator(np.bool_(selection.all(*[name])[mask]))
-            # Save the event numbers of events passing this selection
+
             # Save the event numbers of events passing this selection
             if cfg.RUN.SAVE.PASSING:
                 output['selected_events'][region] += list(df['event'][mask])
