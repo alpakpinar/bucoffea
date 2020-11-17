504e367baa37d96db9548ea4ed07a469a1c76202
Alp Akpinar <aakpinar@bu.edu>
2020-11-17 13:28:22 -0500

    Fixups, update legend and title for the figure

--- bucoffea/plot/studies/fraction_cut_study/plot_jet_pt_dist_endcap.py
+++ bucoffea/plot/studies/fraction_cut_study/plot_jet_pt_dist_endcap.py
@@ -11,12 +11,36 @@ import numpy as np
 from bucoffea.plot.util import merge_datasets, merge_extensions, scale_xs_lumi
 from coffea import hist
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MultipleLocator
 from klepto.archives import dir_archive
 from pprint import pprint
 
 pjoin = os.path.join
 
 warnings.filterwarnings('ignore')
+
+legend_labels = {
+    'DY.*' : "Z$\\rightarrow\\ell\\ell$",
+    'Top.*' : "Top quark",
+    'WJets.*LNu.*' : "W$\\rightarrow\\ell\\nu$",
+    'QCD.*' : "QCD",
+    'Diboson.*' : "WW/WZ/ZZ",
+    'ZJetsToNuNu.*' : "Z$\\rightarrow\\nu\\nu$",
+    'EWKZ.*ZToLL.*' : r"EWK $Z\rightarrow \ell\ell$",
+    'EWKZ.*ZToNuNu.*' : r"EWK $Z\rightarrow \nu\nu$",
+    'EWKW.*' : r"EWK $W\rightarrow \ell\nu$",
+    'VBF_HToInv.*M125.*' : "VBF H(inv)",
+    'MET|Single(Electron|Photon|Muon)|EGamma.*' : "Data"
+
+}
+
+def get_title(distribution, year):
+    if distribution == 'ak4_pt0_eta0':
+        title = r'Leading Jet, $2.5 < |\eta| < 3.0$ {}'
+    elif distribution == 'ak4_pt1_eta1':
+        title = r'Trailing Jet, $2.5 < |\eta| < 3.0$ {}'
+
+    return title.format(year)
 
 def plot_jet_pt(acc, outtag, distribution):
     '''Plot the jet pt distribution for leading or trailing jet (in endcap).'''
@@ -27,9 +51,8 @@ def plot_jet_pt(acc, outtag, distribution):
     scale_xs_lumi(h)
     h = merge_datasets(h)
 
-    # NOTE: Hopefully this works
-    endcap_slice = slice(-3.0,-2.5) + slice(2.5, 3.0)
-    h = h.integrate('jeteta', endcap_slice)
+    endcap_slice = slice(2.5, 3.0)
+    h = h.integrate('jeteta', endcap_slice).integrate('region', 'sr_vbf')
 
     data_err_opts = {
         'linestyle':'none',
@@ -44,20 +67,43 @@ def plot_jet_pt(acc, outtag, distribution):
 
     for year in [2017, 2018]:
         h_data = h[f'MET_{year}']
-        h_mc = h[f'(ZJetsToNuNu.*|EW.*|Top_FXFX.*|Diboson.*|QCD_HT.*|.*DYJetsToLL_M-50_HT_MLM.*|.*WJetsToLNu.*HT.*).*{year}']
-        h_signal = h[f'VBF_HToInv_{year}']
+        h_mc = h[re.compile(f'(ZJetsToNuNu.*|EW.*|Top_FXFX.*|Diboson.*|.*DYJetsToLL_M-50_HT_MLM.*|.*WJetsToLNu.*HT.*).*{year}')]
+        h_signal = h[re.compile(f'VBF_HToInv.*M125.*{year}')]
 
         fig, ax = plt.subplots()
         hist.plot1d(h_data, ax=ax, overlay='dataset', error_opts=data_err_opts)
         hist.plot1d(h_mc, ax=ax, overlay='dataset', stack=True, clear=False)
         hist.plot1d(h_signal, ax=ax, overlay='dataset', clear=False, line_opts=signal_line_opts)
 
+        ax.set_title(get_title(distribution, year))
+
+        # Fix legend labels
+        handles, labels = ax.get_legend_handles_labels()
+        new_labels = []
+        for handle, label in zip(handles, labels):
+            if not ('MET' in label or 'VBF' in label):
+                handle.set_linestyle('-')
+                handle.set_edgecolor('k')
+            
+            for k, v in legend_labels.items():
+                if re.match(k, label):
+                    new_label = v
+            
+            new_labels.append(new_label)
+
+        ax.legend(title='VBF Signal Region', handles=handles, labels=new_labels, ncol=2)
+
+        loc = MultipleLocator(100)
+        ax.xaxis.set_major_locator(loc)
+
         # Save figure
         outdir = f'./output/{outtag}'
         if not os.path.exists(outdir):
             os.makedirs(outdir)
         
-        outpath = pjoin(outdir, f"{distribution.replace('_eta0', '')}_{year}.pdf")
+        disttag = re.sub(r'_eta(\d)', '', distribution)
+
+        outpath = pjoin(outdir, f"{disttag}_{year}.pdf")
         fig.savefig(outpath)
         plt.close(fig)
         print(f'File saved: {outpath}')
