23bf047bcfea12ff376ce4aabe75e4c0ad062803
Alp Akpinar <aakpinar@bu.edu>
2020-11-21 20:23:36 -0500

    Read and save all errors into root file

--- bucoffea/plot/studies/fraction_cut_study/get_combined_endcap_sf.py
+++ bucoffea/plot/studies/fraction_cut_study/get_combined_endcap_sf.py
@@ -27,48 +27,54 @@ def get_combined_sf(fin, outpath):
         edges = fin[f'jetsf_ak40_in_endcap_{year}'].edges
         centers = 0.5 * np.sum(fin[f'jetsf_ak40_in_endcap_{year}'].bins, axis=1)
 
-        # Stat errors
-        sf_combined_statUp = np.abs(fin[f'jetsf_ak40_in_endcap_{year}_statUp'].values - sf_combined)
-        sf_combined_statDown = np.abs(fin[f'jetsf_ak40_in_endcap_{year}_statDown'].values - sf_combined)
-        
-        sf_pos_statUp = np.abs(fin[f'jetsf_ak40_in_pos_endcap_{year}_statUp'].values - sf_pos)
-        sf_pos_statDown = np.abs(fin[f'jetsf_ak40_in_pos_endcap_{year}_statDown'].values - sf_pos)
-        
-        sf_neg_statUp = np.abs(fin[f'jetsf_ak40_in_neg_endcap_{year}_statUp'].values - sf_neg)
-        sf_neg_statDown = np.abs(fin[f'jetsf_ak40_in_neg_endcap_{year}_statDown'].values - sf_neg)
-
         # For the first three bins (p_T < 160 GeV), get individual SF
         # For the last three bins (p_T > 160 GeV), get the combined SF
         new_sf_pos = np.concatenate([sf_pos[:3], sf_combined[3:]])
         new_sf_neg = np.concatenate([sf_neg[:3], sf_combined[3:]])
-
-        # Same for the stat errors
-        new_sf_pos_statUp = np.concatenate([
-            sf_pos_statUp[:3],
-            sf_combined_statUp[3:]
-        ])
-        new_sf_pos_statDown = np.concatenate([
-            sf_pos_statDown[:3],
-            sf_combined_statDown[3:]
-        ])
         
-        new_sf_pos_statErr = np.vstack([new_sf_pos_statUp, new_sf_pos_statDown])
+        # Get all errors
+        err_dict = {
+            'pos_eta' : {},
+            'neg_eta' : {}
+        }
+        for sys in ['stat', 'jesTotal', 'jer', 'pileup', 'prefire']:
+            sf_combined_up = np.abs(fin[f'jetsf_ak40_in_endcap_{year}_{sys}Up'].values - sf_combined)
+            sf_combined_down = np.abs(fin[f'jetsf_ak40_in_endcap_{year}_{sys}Down'].values - sf_combined)
+            
+            sf_pos_up = np.abs(fin[f'jetsf_ak40_in_pos_endcap_{year}_{sys}Up'].values - sf_pos)
+            sf_pos_down = np.abs(fin[f'jetsf_ak40_in_pos_endcap_{year}_{sys}Down'].values - sf_pos)
+            
+            sf_neg_up = np.abs(fin[f'jetsf_ak40_in_neg_endcap_{year}_{sys}Up'].values - sf_neg)
+            sf_neg_down = np.abs(fin[f'jetsf_ak40_in_neg_endcap_{year}_{sys}Down'].values - sf_neg)
 
-        new_sf_neg_statUp = np.concatenate([
-            sf_neg_statUp[:3],
-            sf_combined_statUp[3:]
-        ])
-        new_sf_neg_statDown = np.concatenate([
-            sf_neg_statDown[:3],
-            sf_combined_statDown[3:]
-        ])
+            # Same for the stat errors
+            new_sf_pos_up = np.concatenate([
+                sf_pos_up[:3],
+                sf_combined_up[3:]
+            ])
+            new_sf_pos_down = np.concatenate([
+                sf_pos_down[:3],
+                sf_combined_down[3:]
+            ])
+            
+            # Store the up/down errors in the error dictionary
+            err_dict['pos_eta'][sys] = np.vstack([new_sf_pos_up, new_sf_pos_down])
 
-        new_sf_neg_statErr = np.vstack([new_sf_neg_statUp, new_sf_neg_statDown])
+            new_sf_neg_up = np.concatenate([
+                sf_neg_up[:3],
+                sf_combined_up[3:]
+            ])
+            new_sf_neg_down = np.concatenate([
+                sf_neg_down[:3],
+                sf_combined_down[3:]
+            ])
+    
+            err_dict['neg_eta'][sys] = np.vstack([new_sf_neg_up, new_sf_neg_down])
 
-        # Plot the SF!
+        # Plot the SF (with the stat uncs for now)!
         fig, ax = plt.subplots()
-        ax.errorbar(centers, y=new_sf_pos, yerr=new_sf_pos_statErr, marker='o', label=r'$2.5 < \eta < 3.0$')
-        ax.errorbar(centers, y=new_sf_neg, yerr=new_sf_neg_statErr, marker='o', label=r'$-3.0 < \eta < -2.5$')
+        ax.errorbar(centers, y=new_sf_pos, yerr=err_dict['pos_eta']['stat'], marker='o', label=r'$2.5 < \eta < 3.0$')
+        ax.errorbar(centers, y=new_sf_neg, yerr=err_dict['neg_eta']['stat'], marker='o', label=r'$-3.0 < \eta < -2.5$')
 
         ax.legend()
         ax.set_xlabel(r'Jet $p_T \ (GeV)$')
@@ -84,12 +90,18 @@ def get_combined_sf(fin, outpath):
 
         # Save into output ROOT file
         fout[f'sf_pos_endcap_{year}'] = (new_sf_pos, edges)
-        fout[f'sf_pos_endcap_{year}_statUp'] = (new_sf_pos + new_sf_pos_statUp, edges)
-        fout[f'sf_pos_endcap_{year}_statDown'] = (new_sf_pos + new_sf_pos_statDown, edges)
+
+        # Save the variations as well
+        for sys, stack in err_dict['pos_eta'].items():
+            sf_up, sf_down = stack
+            fout[f'sf_pos_endcap_{year}_{sys}Up'] = (new_sf_pos + sf_up, edges)
+            fout[f'sf_pos_endcap_{year}_{sys}Down'] = (new_sf_pos + sf_down, edges)
 
         fout[f'sf_neg_endcap_{year}'] = (new_sf_neg, edges)
-        fout[f'sf_neg_endcap_{year}_statUp'] = (new_sf_neg + new_sf_neg_statUp, edges)
-        fout[f'sf_neg_endcap_{year}_statDown'] = (new_sf_neg + new_sf_neg_statDown, edges)
+        for sys, stack in err_dict['neg_eta'].items():
+            sf_up, sf_down = stack
+            fout[f'sf_neg_endcap_{year}_{sys}Up'] = (new_sf_neg + sf_up, edges)
+            fout[f'sf_neg_endcap_{year}_{sys}Down'] = (new_sf_neg + sf_down, edges)
 
 def main():
     # Path to the input ROOT file containing individual SFs and their uncertainties
