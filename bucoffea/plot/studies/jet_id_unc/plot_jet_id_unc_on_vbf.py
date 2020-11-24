1a29d0793f1f1c532455d70372fb5b67a3d46147
Alp Akpinar <aakpinar@bu.edu>
2020-11-24 16:28:13 -0500

    Fixups, add aesthetics

--- bucoffea/plot/studies/jet_id_unc/plot_jet_id_unc_on_vbf.py
+++ bucoffea/plot/studies/jet_id_unc/plot_jet_id_unc_on_vbf.py
@@ -9,6 +9,7 @@ from bucoffea.plot.util import merge_datasets, merge_extensions, scale_xs_lumi, 
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MultipleLocator
 from klepto.archives import dir_archive
+from pprint import pprint
 
 pjoin = os.path.join
 
@@ -22,33 +23,49 @@ def plot_jet_id_unc(acc, outtag):
     h = merge_datasets(h)
 
     # Rebin mjj
-    mjj_ax = [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500., 5000.]
+    mjj_ax = hist.Bin('mjj', r'$M_{jj} \ (GeV)$', [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500., 5000.])
     h = h.rebin('mjj', mjj_ax)
             
     for year in [2017, 2018]:
         _h = h.integrate('dataset', re.compile(f'VBF_HToInv.*{year}'))
-        _h = h[re.compile('^sr_vbf_(no_veto|jetsf).*')]
-
-        fig, ax, rax = fig_ratio()
-        hist.plot1d(_h, ax=ax, overlay='dataset')
-
-        ax.set_xlabel('')
-        ax.set_yscale('log')
-        ax.set_ylim(1e-3, 1e5)
-        ax.set_title(f'Jet SF Uncertainties on VBF H(inv): {year}')
-
-        # Plot ratios w.r.t. nominal
         h_nom = _h.integrate('region', 'sr_vbf_no_veto_all')
         h_sfup = _h.integrate('region', 'sr_vbf_jetsfUp')
         h_sfdown = _h.integrate('region', 'sr_vbf_jetsfDown')
 
-        hist.plotratio(h_sfup, h_nom, ax=rax, unc='num', label='Jet SF Up')
-        hist.plotratio(h_sfdown, h_nom, ax=rax, unc='num', label='Jet SF Down', clear=False)
+        fig, ax, rax = fig_ratio()
+        hist.plot1d(h_nom, ax=ax)
+        hist.plot1d(h_sfup, ax=ax, clear=False)
+        hist.plot1d(h_sfdown, ax=ax, clear=False)
+
+        ax.set_xlabel('')
+        # ax.set_yscale('log')
+        # ax.set_ylim(1e-3, 1e5)
+        ax.set_title(f'Jet SF Uncertainties on VBF H(inv): {year}')
+
+        labels = [
+            'Nominal',
+            'Jet ID SF up',
+            'Jet ID SF down',
+        ]
+
+        ax.legend(labels=labels)
+
+        # Plot ratios w.r.t. nominal
+        data_err_opts = {
+            'linestyle':'none',
+            'marker': '.',
+            'markersize': 10.,
+        }
+
+        hist.plotratio(h_sfup, h_nom, ax=rax, unc='num', label='Jet SF Up', error_opts=data_err_opts)
+        hist.plotratio(h_sfdown, h_nom, ax=rax, unc='num', label='Jet SF Down', clear=False, error_opts=data_err_opts)
 
         rax.set_xlabel(r'$M_{jj} \ (GeV)$')
         rax.set_ylabel('Ratio to Nominal')
         rax.set_ylim(0.9,1.1)
         rax.grid(True)
+
+        rax.axhline(1, xmin=0, xmax=1, color='black')
 
         loc = MultipleLocator(0.05)
         rax.yaxis.set_major_locator(loc)
