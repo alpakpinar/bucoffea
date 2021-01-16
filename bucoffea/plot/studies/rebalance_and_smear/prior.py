313d61b770d89a4a81524c95ec8965603b9a06c5
Alp Akpinar <aakpinar@bu.edu>
2021-01-16 11:05:29 -0500

    Fixups, add function to plot priors from 2D histograms

--- bucoffea/plot/studies/rebalance_and_smear/prior.py
+++ bucoffea/plot/studies/rebalance_and_smear/prior.py
@@ -9,6 +9,7 @@ from coffea import hist
 from bucoffea.plot.util import merge_datasets, merge_extensions, scale_xs_lumi
 from matplotlib import pyplot as plt
 from klepto.archives import dir_archive
+from pprint import pprint
 
 pjoin = os.path.join
 
@@ -19,6 +20,14 @@ def preprocess(h, acc, region):
 
     h = h.integrate('region', region)
     return h
+
+def get_plotlabel(region):
+    labels = {
+        'inclusive' : 'Inclusive',
+        'trk_trk' : 'Trk-Trk Events',
+        'not_trk_trk' : 'Non Trk-Trk Events',
+    }
+    return labels[region]
 
 def plot_prior(acc, outtag, distribution, region='inclusive'):
     '''Plot the given distribution (i.e. HTmiss or genMET) in the given region.'''
@@ -31,21 +40,26 @@ def plot_prior(acc, outtag, distribution, region='inclusive'):
 
     # Plot the distribution for both years
     for year in [2017, 2018]:
-        _h = h.integrate('dataset', f'QCD.*{year}')
+        _h = h.integrate('dataset', f'QCD_HT_{year}')
         fig, ax = plt.subplots()
 
         hist.plot1d(_h, ax=ax)
 
         ax.set_yscale('log')
-        ax.set_ylim(1e-2,1e6)
+        ax.set_ylim(1e-2,1e10)
+        ax.set_xlim(0,2000)
 
-        ax.text(0., 1., region,
+        ax.get_legend().remove()
+
+        ax.text(0., 1., get_plotlabel(region),
+            fontsize=14,
             ha='left',
             va='bottom',
             transform=ax.transAxes
         )
 
         ax.text(1., 1., year,
+            fontsize=14,
             ha='right',
             va='bottom',
             transform=ax.transAxes
@@ -56,6 +70,50 @@ def plot_prior(acc, outtag, distribution, region='inclusive'):
         fig.savefig(outpath)
         plt.close(fig)
 
+        print(f'File saved: {outpath}')
+
+def plot_2d_prior(acc, outtag, distribution):
+    acc.load(distribution)
+    # For 2D distributions, we look at inclusive region by default
+    h = preprocess(acc[distribution], acc, region='inclusive')
+
+    outdir = f'./output/{outtag}/2d'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    # Rebin htmiss (use coarser binning)
+    # TODO: Binning to be adjusted
+    new_htmiss_ax = hist.Bin('htmiss', r"$H_{T}^{miss}$ (GeV)", 10, 0, 4000)
+    h = h.rebin('htmiss', new_htmiss_ax)
+
+    # Plot the distribution for both years
+    for year in [2017, 2018]:
+        _h = h.integrate('dataset', f'QCD_HT_{year}')
+        fig, ax = plt.subplots()
+
+        # Plot in bins of HTmiss
+        hist.plot1d(_h, overlay='htmiss', ax=ax)
+        ax.set_yscale('log')
+        ax.set_ylim(1e-2, 1e10)
+
+        ax.text(0., 1., 'QCD MC',
+            fontsize=14,
+            ha='left',
+            va='bottom',
+            transform=ax.transAxes
+        )
+
+        ax.text(1., 1., year,
+            fontsize=14,
+            ha='right',
+            va='bottom',
+            transform=ax.transAxes
+        )
+
+        # Save figure
+        outpath = pjoin(outdir, f'prior_{distribution}_{year}.pdf')
+        fig.savefig(outpath)
+        plt.close(fig)
         print(f'File saved: {outpath}')
 
 def main():
@@ -71,8 +129,21 @@ def main():
         'genmet_pt',
     ]
 
-    for distribution in distributions:
-        plot_prior(acc, outtag, distribution=distribution)
+    regions = [
+        'inclusive',
+        'trk_trk',
+        'not_trk_trk'
+    ]
+
+    # 1D priors
+    for region in regions:
+        for distribution in distributions:
+            plot_prior(acc, outtag, distribution=distribution, region=region)
+
+    # 2D priors
+    for distribution in ['htmiss_met', 'htmiss_ht']:
+        plot_2d_prior(acc, outtag, distribution=distribution)
+
 
 if  __name__ == '__main__':
     main()
